778e0dba12b6d1f7ab03f43e221f37d04cb0d90d
ark kochetkov <akochetkov@ozon.ru>
2019-06-20 19:42:52 +0300

    fib linear

--- demo.hs
+++ demo.hs
@@ -1,5 +1,18 @@
 fibonacci :: Integer -> Integer
-fibonacci n | n == 0 = 0
-            | n == 1 = 1
-            | n > 1 = fibonacci(n - 2) + fibonacci(n - 1)
-            | n < 0 = fibonacci(n + 2) - fibonacci(n + 1)
+fibonacci 0 = 0
+fibonacci 1 = 1
+fibonacci n | n > 0 = incfib 0 1 0 (n - 1)
+      | n < 0 = decfib 0 1 0 (n * (-1) - 1)
+
+incfib :: Integer -> Integer -> Integer -> Integer -> Integer
+incfib cur next it max 
+                    | it > max = cur
+                    | True = incfib (next) (cur + next) (it + 1) max
+
+decfib :: Integer -> Integer -> Integer -> Integer -> Integer
+decfib cur next it max
+                    | it > max = cur * (-1)^max
+                    | True = decfib (next) (cur + next) (it + 1) max
+inc n = incI 0 0 n
+incI acc it n | it > n = acc
+              | True = incI (acc + it) (it + 1) n
