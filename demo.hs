abac43eadb06849f29741cc4d8fa9042cba9fdf0
ark kochetkov <akochetkov@ozon.ru>
2019-06-20 19:39:45 +0300

    fib naive

--- demo.hs
+++ demo.hs
@@ -1,3 +1,5 @@
-doubleFact :: Integer -> Integer
-doubleFact n | n < 2 = 1
-             | otherwise = n * doubleFact (n - 2)
+fibonacci :: Integer -> Integer
+fibonacci n | n == 0 = 0
+            | n == 1 = 1
+            | n > 1 = fibonacci(n - 2) + fibonacci(n - 1)
+            | n < 0 = fibonacci(n + 2) - fibonacci(n + 1)
