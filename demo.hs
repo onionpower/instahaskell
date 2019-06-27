3c104c8c026cae48e626fa3f65221436a03fc726
onionpower <chocoonion@gmail.com>
2019-06-27 20:52:48 +0300

    (sum, digits num)

--- demo.hs
+++ demo.hs
@@ -1,11 +1,6 @@
---a0 = 1, a1 = 2, a2 = 3, a(k+3) = a(k+2) + a(k+1) - 2*ak
-
-seqA :: Integer -> Integer
-seqA 0 = 1
-seqA 1 = 2
-seqA 2 = 3
-seqA n = let
-      seqI n2 n1 n it max | it > max = n2 + n1 - 2*n
-                          | True = seqI (n2 + n1 - 2*n) n2 n1 (it + 1) max
-      in
-            seqI 3 2 1 4 n
+sum'n'count :: Integer -> (Integer, Integer)
+sum'n'count x | x < 0 = sc (abs x) 0 0
+              | otherwise = sc x 0 0
+sc :: Integer -> Integer -> Integer -> (Integer, Integer)
+sc x sum count | x > 0 = sc (quot x 10) (sum + (mod x 10)) (count + 1)
+               | otherwise = (sum, count)
