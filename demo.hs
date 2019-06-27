fa58668078cb42e2537bb38944b035bfab51b1a7
onionpower <chocoonion@gmail.com>
2019-06-27 16:08:35 +0300

    a sequence

--- demo.hs
+++ demo.hs
@@ -4,14 +4,8 @@ seqA :: Integer -> Integer
 seqA 0 = 1
 seqA 1 = 2
 seqA 2 = 3
-seqA n = seqI 3 2 1 4 n
-
-seqI :: Integer -> Integer -> Integer -> Integer -> Integer -> Integer
-seqI n2 n1 n it max | it > max = n2 + n1 + 2*n
-                    | True = seqI (n2 + n1 + 2*n) n2 n1 (it + 1) max
-
-seqB :: Integer -> Integer
-seqB 0 = 1
-seqB 1 = 2
-seqB 2 = 3
-seqB n =  2 * seqB (n - 3) + seqB (n - 2) + seqB (n - 1)
+seqA n = let
+      seqI n2 n1 n it max | it > max = n2 + n1 - 2*n
+                          | True = seqI (n2 + n1 - 2*n) n2 n1 (it + 1) max
+      in
+            seqI 3 2 1 4 n
