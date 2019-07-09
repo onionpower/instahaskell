bcb1f649e84b8a752d9d17387ce16d1e3b3093a3
onionpower <chocoonion@gmail.com>
2019-07-09 18:45:25 +0300

    sum'n'count

--- demo.hs
+++ demo.hs
@@ -1,10 +1,8 @@
 sum'n'count :: Integer -> (Integer, Integer)
-sum'n'count x | x == 0 = (0, 1)
-              | x < 0 = sc (abs x) 0 0
-              | otherwise = sc x 0 0
-sc :: Integer -> Integer -> Integer -> (Integer, Integer)
-sc x sum count | x > 0 = sc (quot x 10) (sum + (mod x 10)) (count + 1)
-               | otherwise = (sum, count)
+sum'n'count x = sc (abs x) 0 0 
+    where
+        sc x sum count | x < 10 = (sum + x, count + 1)
+                       | otherwise = sc (quot x 10) (sum + (mod x 10)) (count + 1)
 
 integration :: (Double -> Double) -> Double -> Double -> Double
 integration f a b = 
