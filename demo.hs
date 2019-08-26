8d83a7dd64dc71ea998e96c1f04c9f02aa1e99ef
onionpower <chocoonion@gmail.com>
2019-08-26 23:16:32 +0300

    wip

--- demo.hs
+++ demo.hs
@@ -1,16 +1,14 @@
-sum'n'count :: Integer -> (Integer, Integer)
-sum'n'count x = sc (abs x) 0 0 
-    where
-        sc x sum count | x < 10 = (sum + x, count + 1)
-                       | otherwise = sc (quot x 10) (sum + (mod x 10)) (count + 1)
-
 integration :: (Double -> Double) -> Double -> Double -> Double
 integration f a b = 
-    let fa = f a
-        fb = f b
-        square a b = (b - a) * (fa + fb)/2
-    in square a b
+    let
+        acc = 0        
+        (l, r, sign) = if a < b then (a, b, 1) else (b, a, -1)
+        inc = (r - l)/1000
+    in (sign * simit acc inc l (l + inc) f r) / 2
 
-swap :: (a, b) -> (b, a)
-swap (x, y) = (y, x)
-    
+simit :: Double -> Double -> Double -> Double -> (Double -> Double) -> Double -> Double
+simit acc inc l r f b
+        | r > b = acc
+        | otherwise = simit (acc + trapsqr f l r) inc (l + inc) (r + inc) f b
+        where
+            trapsqr f l r = (r - l) * (f l + f r)
