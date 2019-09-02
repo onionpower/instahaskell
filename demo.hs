4f26796fa29115204a165caa724327e08611d7e2
onionpower <chocoonion@gmail.com>
2019-09-02 22:04:09 +0300

    wip

--- demo.hs
+++ demo.hs
@@ -4,11 +4,11 @@ integration f a b =
         acc = 0        
         (l, r, sign) = if a < b then (a, b, 1) else (b, a, -1)
         inc = (r - l)/1000
-    in (sign * simit acc inc l (l + inc) f r) / 2
+    in sign*inc*((f l + f r)/2 + simit acc inc (l + inc) (r - inc) f)
 
-simit :: Double -> Double -> Double -> Double -> (Double -> Double) -> Double -> Double
-simit acc inc l r f b
-        | r > b = acc
-        | otherwise = simit (acc + trapsqr f l r) inc (l + inc) (r + inc) f b
+simit :: Double -> Double -> Double -> Double -> (Double -> Double) -> Double
+simit acc inc l r f
+        | l > r = acc
+        | otherwise = simit (acc + f l) inc (l + inc) r f
         where
-            trapsqr f l r = (r - l) * (f l + f r)
+            it f l = f l
