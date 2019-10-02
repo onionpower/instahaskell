97d968516952c3bebf1b42f2ca5ac126aae42c1e
onionpower <chocoonion@gmail.com>
2019-10-02 17:21:29 +0300

    wip

--- demo.hs
+++ demo.hs
@@ -1,14 +1,13 @@
 integration :: (Double -> Double) -> Double -> Double -> Double
-integration f a b = 
-    let
-        acc = 0        
-        (l, r, sign) = if a < b then (a, b, 1) else (b, a, -1)
-        inc = (r - l)/1000
-    in sign*inc*((f l + f r)/2 + simit acc inc (l + inc) (r - inc) f)
+integration f a b
+    | a == b = 0 
+    | otherwise = sign*inc*((f l + f r)/2 + simit acc inc (l + inc) (r - inc) f)
+        where
+            acc = 0        
+            (l, r, sign) = if a < b then (a, b, 1) else (b, a, -1)
+            inc = (r - l)/1024
 
 simit :: Double -> Double -> Double -> Double -> (Double -> Double) -> Double
 simit acc inc l r f
         | l > r = acc
         | otherwise = simit (acc + f l) inc (l + inc) r f
-        where
-            it f l = f l
---
+++ getsecondfrom.hs
@@ -0,0 +1,6 @@
+getSecondFrom :: p0 -> p1 -> p2 -> p1
+getSecondFrom a b c = b
+
+lul :: a -> a -> b -> a -> a
+lul a b c d = a
+lul a b c d = b
